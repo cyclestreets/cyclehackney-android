be83c5e4abca7c8107b702406a71df77d21486c5
Jez Higgins <jez@jezuk.co.uk>
2014-01-27 19:43:19 +0000

    Swapped out journey planner, swapped in photomap

--- cyclehackney/src/main/java/uk/gov/hackney/CycleHackney.java
+++ cyclehackney/src/main/java/uk/gov/hackney/CycleHackney.java
@@ -1,15 +1,10 @@
 package uk.gov.hackney;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.widget.TabHost;
 
 import net.cyclestreets.MainTabbedActivity;
-import net.cyclestreets.RouteMapFragment;
+import net.cyclestreets.PhotoMapFragment;
 import net.cyclestreets.AboutFragment;
 import net.cyclestreets.fragments.R;
 
@@ -29,8 +24,8 @@ public class CycleHackney extends MainTabbedActivity
 
   protected void addTabs(final TabHost tabHost) {
     addTab("Journey Log", R.drawable.ic_menu_live_ride, LogJourneyFragment.class);
-    addTab("Route Map", R.drawable.ic_tab_planroute, RouteMapFragment.class);
-    //addTab("Photomap", R.drawable.ic_tab_photomap, PhotoMapFragment.class);
+    //addTab("Route Map", R.drawable.ic_tab_planroute, RouteMapFragment.class);
+    addTab("Photomap", R.drawable.ic_tab_photomap, PhotoMapFragment.class);
     addTab("About", R.drawable.ic_tab_more, AboutFragment.class);
   } // addTabs
 } // class CycleHackney
