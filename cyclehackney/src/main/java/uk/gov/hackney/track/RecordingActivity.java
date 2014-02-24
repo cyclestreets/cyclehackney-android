10a352409d73eefdb1a3f1a7447b0a1995bafa4f
Jez Higgins <jez@jezuk.co.uk>
2014-02-24 09:23:42 +0000

    lock on location while recording

--- cyclehackney/src/main/java/uk/gov/hackney/track/RecordingActivity.java
+++ cyclehackney/src/main/java/uk/gov/hackney/track/RecordingActivity.java
@@ -54,7 +54,7 @@ public class RecordingActivity extends Activity
 
     mapView_ = new CycleMapView(this, getClass().getName());
     mapView_.hideLocationButton();
-    mapView_.enableAndFollowLocation();
+    mapView_.lockOnLocation();
     mapView_.getController().setZoom(16);
 
     final RelativeLayout v = (RelativeLayout)findViewById(R.id.mapholder);
