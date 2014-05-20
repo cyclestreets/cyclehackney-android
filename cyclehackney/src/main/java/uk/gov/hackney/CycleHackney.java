f2d3c005ae013af8aac0ac07c8b059901ee6918c
Jez Higgins <jez.higgins@thomsonreuters.com>
2014-05-20 20:34:23 +0100

    added photoupload tab

--- cyclehackney/src/main/java/uk/gov/hackney/CycleHackney.java
+++ cyclehackney/src/main/java/uk/gov/hackney/CycleHackney.java
@@ -12,6 +12,7 @@ import android.widget.TabHost;
 import net.cyclestreets.MainTabbedActivity;
 import net.cyclestreets.PhotoMapFragment;
 import net.cyclestreets.AboutFragment;
+import net.cyclestreets.PhotoUploadFragment;
 import net.cyclestreets.fragments.R;
 
 import java.util.ArrayList;
@@ -81,6 +82,7 @@ public class CycleHackney extends MainTabbedActivity {
   protected void addTabs(final TabHost tabHost) {
     addTab("Journey Log", R.drawable.ic_menu_live_ride, LogJourneyFragment.class);
     //addTab("Route Map", R.drawable.ic_tab_planroute, RouteMapFragment.class);
+    addTab("Upload", R.drawable.ic_tab_photoupload, PhotoUploadFragment.class);
     addTab("Photomap", R.drawable.ic_tab_photomap, PhotoMapFragment.class);
     addTab("About", R.drawable.ic_tab_more, AboutFragment.class);
   } // addTabs
